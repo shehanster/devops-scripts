ac277c9dfe5fdc334beab2d7edf1c82a483cd16a
Shehan Marino <shehanster@hotmail.com>
2017-07-28 17:18:48 +0530

    Update mysqldump-to-csv.sql

--- mysqldump-to-csv.sql
+++ mysqldump-to-csv.sql
@@ -1,6 +1,6 @@
 # This script gets mysql table to csv 
 # To use you need to run this on bash using the collosing command "mysql -h "server-name" -u "root" "-pXXXXXXXX" "database-name" < "mysqldump-to-csv.sql""
-
+# replace t1 with table name PREFIX for the file name
 
 SET @TS = DATE_FORMAT(NOW(),'_%Y_%m_%d_%H_%i_%s');
  
@@ -8,7 +8,7 @@ SET @FOLDER = '/tmp/';
 SET @PREFIX = 'name';
 SET @EXT    = '.csv';
  
-SET @CMD = CONCAT("SELECT * FROM orders INTO OUTFILE '",@FOLDER,@PREFIX,@TS,@EXT,
+SET @CMD = CONCAT("SELECT * FROM t1 INTO OUTFILE '",@FOLDER,@PREFIX,@TS,@EXT,
     "' FIELDS ENCLOSED BY '\"' TERMINATED BY ';' ESCAPED BY '\"'",
     "  LINES TERMINATED BY '\r\n';");
  
