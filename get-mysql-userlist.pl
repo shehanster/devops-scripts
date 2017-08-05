cc9e9f5f24b214945976e0d0fdc34698d4d66862
Shehan Marino <shehanster@hotmail.com>
2017-08-05 19:21:24 +0530

    Create get-mysql-userlist.pl
    added script to run against with root credentials or superuser credentials to get user and host with their grants to display on CLI

---
+++ get-mysql-userlist.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl -w
+use DBI;
+$dbh = DBI->connect("DBI:mysql:database=mysql;host=mysql1.mydomain.com", "user", "password",{'RaiseError' => 1});
+$sql = "select user,host from user";
+$sth = $dbh->prepare($sql);
+$sth->execute or die "SQL Error: $DBI::errstr\n";
+while (@row = $sth->fetchrow_array) {
+   $sql = "SHOW GRANTS FOR '$row[0]'\@'$row[1]'\n";
+   $sthu = $dbh->prepare($sql);
+   $sthu->execute or die "SQL Error: $DBI::errstr\n";
+   while(@results = $sthu->fetchrow_array){
+       foreach $i (@results){
+           print "$i;\n";
+   }
+}
+}
